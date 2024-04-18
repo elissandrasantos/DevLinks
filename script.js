b671e8d16187c0b4b1194a30ca92ba5b63f38e02
Elissandra Santos <elissandra319@gmail.com>
2024-04-18 14:36:16 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Elissandra de cabelos ondulados e óculos escuros."
+    )
+
+  } else {
+    // se tiver sem light mode, manter a iamgem normal
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Elissandra de cabelos escovados, maquiagem leve e sorrindo."
+    )
+  }
+}
